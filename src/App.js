5ca52d18f4411cc9015facdffa3c54c6f3f4bcd1
Damo444 <dr.adhikari19@live.com>
2021-12-18 15:34:51 +0600

    first change

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "@material-ui/core/Button";
+function App() {
+  return (
+    <div className="App">
+      <Button 
+        style={{
+          fontSize:24
+        }}
+        variant='contained'
+        color="primary">
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+    
+    
+    </div>
+  );
+}
+
+export default App;
