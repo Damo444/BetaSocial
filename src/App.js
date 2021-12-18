66c87253d942cd705368ea2b0e926a7f027c7c0d
Damo444 <82561662+Damo444@users.noreply.github.com>
2021-12-18 17:07:17 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,33 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {data } from './data/data';
-
-import Card from './components/Card';
-
-const colorsToPick=[
-  {
-    label:'blue', 
-    color:'#40BBFF',
-  },
-  {
-    label:'green', 
-    color:'#50E3C2',
-  },
-  {
-    label:'pink', 
-    color:'#DF70B5',
-  },
-  
-  {
-    label:'red',
-    color:'#be2a45',
-},
-  {
-    label:'yellow', 
-    color:'#FEB102',
-  }
-
-]
-
-class App extends Component {
-  constructor(){
-    super();
-    this.state={
-      title:'',
-      description:'',
-      selectedColor: 'blue',
-    }
-  }
-
-
-  
-
-  render(){
-   
-    
-  return (   
+import Button from "@material-ui/core/Button";
+function App() {
+  return (
     <div className="App">
-      
-      <h1> what </h1>
-
-     
-
-     
-
+      <Button 
+        style={{
+          fontSize:25
+        }}
+        variant='contained'
+        color="primary">
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+      <Button variant='contained'>
+        Home
+      </Button>
+    
+    
     </div>
   );
 }
-}
-
-
-
-
 
 export default App;
